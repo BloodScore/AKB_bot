2afd432f478d07bb16910a3ac2db4d0c6559a38d
BloodScore <dmitrygolysh@gmail.com>
2021-07-22 15:04:18 +0300

    requirements.txt added

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from telebot import TeleBot, types
 from utils import short_url, get_random_anek_page, get_single_anek, get_comments
 from dotenv import load_dotenv
 
-
 load_dotenv()
 bot = TeleBot(os.environ.get('BOT_API_KEY'))
 users_schedules = {}
@@ -38,7 +37,9 @@ def send_random_anek(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == 'no':
-        bot.send_message(call.message.chat.id, '/anek - случайный анекдот\n/anek_of_the_day - лучший анекдот раз в день')
+        bot.send_message(call.message.chat.id, '/anek - случайный анекдот\n'
+                                               '/anek_of_the_day - анекдот раз в день\n'
+                                               '/unsubscribe - отписаться от рассылки')
     else:
         comments = ''
         for comment in get_comments(get_random_anek_page(call.data)[0]):
@@ -48,19 +49,22 @@ def callback_worker(call):
 
 @bot.message_handler(commands=['anek_of_the_day', 'anek_of_the_day@my_akb_bot'])
 def send_anek_of_the_day(message):
-    user_schedule = schedule.Scheduler()
-    job = user_schedule.every(10).minutes.do(send_anek, message)
+    if users_schedules.get(message.chat.id):
+        bot.send_message(message.chat.id, 'Вы уже подписаны на рассылку')
+    else:
+        user_schedule = schedule.Scheduler()
+        job = user_schedule.every(10).minutes.do(send_anek, message)
 
-    users_schedules[message.chat.id] = [user_schedule, job]
+        users_schedules[message.chat.id] = [user_schedule, job]
 
-    bot.send_message(message.chat.id, 'Вы подписались на рассылку анекдотов.')
+        bot.send_message(message.chat.id, 'Вы подписались на рассылку анекдотов.')
 
-    def schedule_checker():
-        while True:
-            user_schedule.run_pending()
-            sleep(1)
+        def schedule_checker():
+            while True:
+                user_schedule.run_pending()
+                sleep(1)
 
-    Thread(target=schedule_checker).start()
+        Thread(target=schedule_checker).start()
 
 
 @bot.message_handler(commands=['unsubscribe', 'unsubscribe@my_akb_bot'])
